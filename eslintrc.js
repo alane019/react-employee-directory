27efc04efbe347ad8f187117845cb095858d56e6
Adam L <alane019@gmail.com>
2021-03-09 00:09:14 -0500

    add eslint.js for jsx rule

---
+++ eslintrc.js
@@ -0,0 +1,14 @@
+// The example settings below tell ESLint use the eslint-plugin-react linting rules, and manually configures two rules.
+// https://github.com/yannickcr/eslint-plugin-react
+module.exports = {
+    plugins: ["react"],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true
+      }
+    },
+    rules: {
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error"
+    }
+  };
