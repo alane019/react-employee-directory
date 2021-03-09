964e0e74046d83f947b9e00736cfbd6474c5ee77
Adam L <alane019@gmail.com>
2021-03-09 09:22:58 -0500

    update API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 let resultLimit = 5;
+// expected example: https://randomuser.me/api/?results=5
 const url = `https://randomuser.me/api/?results=${resultLimit}`;
+
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  search: function() {
-    let employees = axios.get(url);
-    console.log(employees);
-    return employees;
+  search: function(query) {
+    //let employees = axios.get(url);
+   // console.log(employees);
+   console.log({query});
+    return  axios.get(url);;
   }
 };
