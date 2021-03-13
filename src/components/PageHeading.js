0f35aa38eff8107cfc2e363ab5646fb281161ce5
Adam L <alane019@gmail.com>
2021-03-13 12:58:43 -0500

    page heading updates

--- src/components/PageHeading.js
+++ src/components/PageHeading.js
@@ -3,7 +3,7 @@ import React from "react";
 
 function PageHeading() {
   return (
-    <div style={{padding: 15}}className="jumbotron">
+    <div style={{padding: 15}} id="top" className="jumbotron">
       <p style={{fontSize: 25, marginBottom: 0}} className="display-4">Your Organization's Directory</p>
       <p style={{color:"gray", paddingLeft: 12, marginTop: 0}} className="lead">{
       (new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'short' }).format())
