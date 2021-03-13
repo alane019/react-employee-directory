cfdb21e3e5ba2b3912ca62648da238312b6e75c0
Adam L <alane019@gmail.com>
2021-03-12 21:41:32 -0500

    move api to directory container, updates to employee details + container

--- src/components/DirectoryContainer.js
+++ src/components/DirectoryContainer.js
@@ -1,63 +1,71 @@
 //DirectoryContainer is the main container for page content. Includes all excecpt heading.
 // export to app.js to display below the PageHeading component on the index page
-import React from "react";
-import API from "../utils/API";
+import React, { Component } from "react";
 import Row from "./Row";
 import Container from "./Container";
 import axios from "axios";
-let resultLimit = 5;
-// expected example: https://randomuser.me/api/?results=5
-const url = `https://randomuser.me/api/?results=${resultLimit}`;
-console.log("url after template literal" + url);
+import EmployeeDetail from "./EmployeeDetail";
+
+const url = `https://randomuser.me/api/?results=2`;
 
 // eslint-disable-next-line import/no-anonymous-default-export
-
-  let axiosSearch = () => {
     //let employees = axios.get(url);
    // console.log(employees);
-    return  axios.get(url);
-  };
+   console.log(" ------------------------------ ");
+axios.get(url)
+    .then( res => {
+      console.log(" - - - - -  - - - - -  - - - - - ")
+      console.log(console.log(Array.from(res.data.results)));
+      console.log(console.dir(res));
+      console.log(" - - - - -  - - - - -  - - - - - ")
+      console.log(res.data.results[0]);
+      return(res.data.results);
+    })
+    console.log(" ------------------------------ ");
 
-
-
-class DirectoryContainer extends React.Component {
+class DirectoryContainer extends Component {
   state = {
-    result: {},
-    search: "",
+    result: {}
   };
 
   // react lifecycle function: runs when component mounts
   componentDidMount() {
-    console.log({ Container });
     this.getEmployees();
-    console.log("EMPLOYEE DATA:  \n " + this.getEmployees());
+    //console.log("EMPLOYEE DATA:  \n " + this.getEmployees().results);
   }
 
   // return randomized list of employees
+
   getEmployees = () => {
-    axiosSearch.search()
-      .then(console.log(" Axios API Result: \n "))
-      .then((res) => this.setState({ result: res.data }))
+    axios.get(url)
+      .then((res) => {
+        return this.setState({ result: res.data });
+      })
       .catch((err) => console.log(err));
   };
 
-
   render() {
     return (
-        <div className="container">
+      <Container>
           <Row>
             <div className="col md-8">
               <img alt="place holder" src=".\screen-main.JPG"></img>
+              {console.log("THIS is >>> "  + this )}
+             <EmployeeDetail
+                key={this.email}
+                city={this.location.city.value}  
+              />
             </div>
-            </Row>
-        </div>
+          </Row>
+      </Container>
     );
   }
-
 }
-
 export default DirectoryContainer;
 
-
+/*
+   state={this.state.result.State}
+   country={this.state.result.Country}
+*/
 
 
--- src/components/EmployeeDetail.js
+++ src/components/EmployeeDetail.js
@@ -1,18 +1,29 @@
 import React from "react";
 
 function EmployeeDetail(props) {
+
   return (
     <div className="text-center">
-      <img
-        alt="employee"
-        className="img-fluid"
-        src={props.thumbnail}
-        style={{ margin: "0 auto" }}
-      />
-      <h3>City: {props.city}</h3>
-      <h3>State: {props.state}</h3>
-      <h3>Country: {props.country}</h3>
+      <h3>State: {props.location.city}   </h3>
+
+      { /*
+       <ul>   {props.map((itm, idx) => (<li key={idx}> {itm}  </li>))}</ul>
+    
+      { response.map((item, index) => (<li key={index}>{item}</li>)) }
+       */ }
     </div>
   );
 }
 export default EmployeeDetail;
+
+/*
+    
+
+      <h3>Country: {props.location.country}</h3>
+      <img
+        alt="employee"
+        className="img-fluid"
+        src={props.picture.thumbnail}
+        style={{ margin: "0 auto" }}
+      />
+  */
